3b4e0e16df899d04c85b7be9d0e096933b02da9c
Andrew Miner <asminer@iastate.edu>
2022-06-07 20:46:37 -0500

    Decent user interface for functions

--- src/forest.h
+++ src/forest.h
@@ -63,6 +63,7 @@ struct rexdd_forest_s {
     rexdd_unique_table_t UT;
 
     // ...
+
     struct rexdd_function_s *roots;
 };
 
@@ -105,7 +106,7 @@ void rexdd_reduce_edge(
 
 /********************************************************************
  *
- *  Top-level, easy to use functions.
+ *  Top-level, easy to use function interface.
  *
  ********************************************************************/
 
@@ -123,7 +124,41 @@ typedef struct rexdd_function_s     rexdd_function_t;
 typedef struct rexdd_function_s*    rexdd_function_p;
 
 /*
- *  Initialize.  Just fill with zeroes.
+ *  TBD
+ *
+ *  Add a function node to its owner's list of roots.
+ *
+ */
+static inline void rexdd_add_to_forest_roots(rexdd_function_p f)
+{
+    // TBD.
+    // Add node f to the front of f->owner's list of roots,
+    // and update doubly-linked list pointers.
+}
+
+/*
+ *  TBD
+ *
+ *  Remove a function node from its owner's list of roots.
+ *
+ */
+static inline void rexdd_remove_from_forest_roots(rexdd_function_p f)
+{
+    //
+    // TBD.
+    // update f->prev, f->next, and other pointers.
+    // Might need to update f->owner's list pointer.
+
+    f->owner = 0;
+    f->prev = 0;
+    f->next = 0;
+}
+
+
+/*
+ *  Initialize a function struct.
+ *
+ *  TBD: probably should throw a runtime error if f is null.
  */
 static inline void rexdd_init_function(rexdd_function_p f)
 {
@@ -134,22 +169,71 @@ static inline void rexdd_init_function(rexdd_function_p f)
     }
 }
 
-/*
- *  Overwrite a function and update lists appropriately.
- *      Same forest - just overwrite the root.
- *      Different forest - remove from old forest's list of roots,
- *          add to new forest list of roots (unless new forest is null).
- */
-void rexdd_set_function(rexdd_function_p fn, rexdd_forest_p For, rexdd_edge_t r);
 
 /*
  * Done with a function.
  */
 static inline void rexdd_done_function(rexdd_function_p f)
 {
-    rexdd_edge_t dummy;
-    rexdd_set_function(f, 0, dummy);
+    if (f) {
+        rexdd_remove_from_forest_roots(f);
+    }
+
 }
+
+
+
+/*
+ *  Build a function of a single variable,
+ *  using an already initialized function struct.
+ *      @param  fn      function struct, specifies forest to use
+ *                      and the function will be updated in place.
+ *      @param  v       Variable level.  If 0, will build
+ *                      a constant function 'false'.
+ *      @param  c       If true, complement the variable
+ *                      (i.e., build !v instead of v).
+ */
+void rexdd_reset_as_variable(
+        rexdd_function_p    fn,
+        uint32_t            v,
+        bool                c);
+
+
+/*
+ *  Initialize a function struct,
+ *  and fill it with a function of a single variable.
+ *      @param  fn      uninitialized function struct on input;
+ *                      will be initialized and filled with the
+ *                      desired function.
+ *      @param  For     Forest to use.
+ *      @param  v       Variable level.  If 0, will build
+ *                      a constant function 'false'.
+ *      @param  c       If true, complement the variable
+ *                      (i.e., build !v instead of v).
+ */
+void rexdd_init_as_variable(
+        rexdd_function_p    fn,
+        rexdd_forest_p      For,
+        uint32_t            v,
+        bool                c);
+
+
+
+/*
+ *  If-then-else operator.
+ *      @param  f       Function f, already set.
+ *      @param  g       Function g, already set, in same forest as f.
+ *      @param  h       Function h, already set, in same forest as f.
+ *      @param  result  In: initialized function struct.
+ *                      Out: result of ITE(f, g, h), which is a new
+ *                      function in the same forest as f, g, h, where
+ *                      result(x) = f(x) ? g(x) : h(x).
+ */
+void rexdd_ITE(
+        const rexdd_function_p f,
+        const rexdd_function_p g,
+        const rexdd_function_p h,
+              rexdd_function_p result);
 
 
 #endif
