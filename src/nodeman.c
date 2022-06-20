e97e7ebbdcfb182ebab094bf631d4fa29c7ac2e1
Andrew Miner <asminer@iastate.edu>
2022-06-19 19:57:42 -0500

    Draft of get handle.  Need to cleanup old stuff.

--- src/nodeman.c
+++ src/nodeman.c
@@ -38,6 +38,8 @@ void rexdd_init_nodeman(rexdd_nodeman_p M, unsigned maxpages)
         rexdd_zero_nodepage(M->pages+i-1, M->empty_pages);
         M->empty_pages = i;
     }
+
+    M->previous_handle = 0;
 }
 
 
@@ -68,6 +70,104 @@ void rexdd_free_nodeman(rexdd_nodeman_p M)
     M->pages_size = 0;
     M->not_full_pages = 0;
     M->empty_pages = 0;
+    M->previous_handle = 0;
 }
 
+
+/****************************************************************************
+ *
+ *  Get a node handle, and fill it with an unpacked node.
+ *      @param  M           Node manager
+ *      @param  n           Unpacked node
+ *
+ *      @return A node handle that now stores n.
+ *
+ */
+rexdd_node_handle_t
+rexdd_nodeman_get_handle(rexdd_nodeman_p M, const rexdd_unpacked_node_p n)
+{
+    rexdd_sanity1(M, "Null node manager");
+    rexdd_sanity1(M->pages, "Empty node manager");
+
+    //
+    // 64 bit to avoid overflows while shifting
+    //
+    uint_fast64_t pnum, slot;
+
+    //
+    // Re-use the recycled handle, if we have one.
+    //
+    if (M->previous_handle) {
+        const rexdd_node_handle_t h = M->previous_handle-1;
+
+        slot = h & 0x00ffffff;
+        pnum = h >> 24;
+
+        rexdd_sanity2(pnum < M->max_pages, "Bad page %llu in handle", pnum);
+        rexdd_unpacked_to_packed(u, M->pages[pnum].chunk+slot);
+        return h;
+    }
+
+    if (0==M->not_full_pages) {
+        //
+        // List of partially filled pages is empty.
+        // Make a new one from the list of empty pages.
+        //
+
+        if (!M->empty_pages) {
+            //
+            // No empty pages.  That means we've exhausted the array.
+            // Time to enlarge, if we can.
+            //
+            if (M->pages_size >= M->max_pages) {
+                // We can't enlarge.
+                rexdd_error(__FILE__, __LINE__,
+                    "Out of nodes in node manager; %lu pages are full", M->max_pages);
+                return 0;   // Shouldn't get here
+            }
+
+            // Enlarge
+            uint_fast32_t nps = M->pages_size * 2;
+            if (nps > M->max_pages) {
+                nps = M->max_pages;
+            }
+            M->pages = realloc(M->pages, nps * sizeof(rexdd_nodepage_t));
+            rexdd_check1(M->pages, "realloc fail in nodeman");
+
+            // zero out the new pages, add them to the empty list
+            unsigned i;
+            for (i=nps; i>M->pages_size; i--) {
+                rexdd_zero_nodepage(M->pages+i-1, M->empty_pages);
+                M->empty_pages = i;
+            }
+            M->pages_size = nps;
+        }
+
+        //
+        // Remove a page from the empty list, add it to the not full list,
+        // and allocate the page.
+        //
+        pnum = M->empty_pages-1;
+        M->empty_pages = M->pages[pnum].next;
+        M->pages[pnum].next = 0;
+        M->not_full_pages = pnum+1;
+        rexdd_init_nodepage(M->pages + pnum);
+    }
+
+    //
+    // Use the list of pages with free slots.
+    //
+    pnum = M->not_full_pages-1;
+    slot = rexdd_page_free_slot(M->pages + pnum);
+
+    //
+    // If the page is full now, remove it from the list
+    //
+    if (0==M->pages[pnum].num_unused) {
+        M->not_full_pages = M->pages[pnum].next;
+    }
+
+    rexdd_unpacked_to_packed(u, M->pages[pnum].chunk+slot);
+    return 1+ (pnum << 24 | slot);
+}
 
--- src/nodeman.h
+++ src/nodeman.h
@@ -2,6 +2,7 @@
 #define NODEMAN_H
 
 #include "nodepage.h"
+#include "error.h"
 
 /*
  *  Efficient storage of lots of nodes, for use by a forest.
@@ -15,7 +16,7 @@
  *  Zero is used as a null handle.
  *
  */
-typedef uint_fast64_t  rexdd_node_handle;
+typedef uint_fast64_t  rexdd_node_handle_t;
 
 
 /****************************************************************************
@@ -46,6 +47,9 @@ typedef struct {
     /* list of not allocated pages */
     uint_fast32_t empty_pages;
 
+    /* Last recycled handle, or 0. */
+    rexdd_node_handle_t previous_handle;
+
 } rexdd_nodeman_t;
 
 typedef rexdd_nodeman_t* rexdd_nodeman_p;
@@ -73,40 +77,67 @@ void rexdd_init_nodeman(rexdd_nodeman_p M, unsigned maxpages);
 void rexdd_free_nodeman(rexdd_nodeman_p M);
 
 
+/****************************************************************************
+ *
+ *  Get a node handle, and fill it with an unpacked node.
+ *      @param  M           Node manager
+ *      @param  n           Unpacked node
+ *
+ *      @return A node handle that now stores n.
+ *
+ */
+rexdd_node_handle_t
+rexdd_nodeman_get_handle(rexdd_nodeman_p M, const rexdd_unpacked_node_p n);
+
+
+/****************************************************************************
+ *
+ *  Recycle the node handle (and node) just returned by
+ *  rexdd_nodeman_get_handle().  We can only have one recycled handle at a time.
+ *
+ *      @param  M           Node manager
+ *      @param  h           Node handle to recycle
+ *
+ */
+static inline
+void rexdd_nodeman_reuse(rexdd_nodeman_p M, rexdd_node_handle_t h)
+{
+    rexdd_sanity1(M, "Null node manager");
+    rexdd_sanity1(0==M->previous_handle, "Too many reused node manager handles");
+
+    M->previous_handle = h;
+}
+
+
+/****************************************************************************
+ *
+ *  Fill in an unpacked node from a node handle.
+ *
+ *      @param  M           Node manager
+ *      @param  h           Node handle
+ *      @param  u           Unpacked node to fill
+ *
+ */
+static inline
+void rexdd_unpack_handle(const rexdd_nodeman_p M, rexdd_node_handle_t h,
+        rexdd_unpacked_node_p u)
+{
+    rexdd_sanity1(M, "Null node manager");
+    rexdd_sanity1(M->pages, "Empty node manager");
+    rexdd_sanity1(h, "Null handle in unpack");
+
+    --h;
+    const uint_fast32_t slot = h & 0x00ffffff;
+    const uint_fast32_t pnum = h >> 24;
+
+    rexdd_sanity2(pnum < M->max_pages, "Bad page %lu in handle", pnum);
+    rexdd_packed_to_unpacked(M->pages[pnum].chunk+slot, u);
+}
+
 //
 // OLD BELOW HERE
 //
 
-/*
- *  TBD - compaction?
- *      need a renumbering array
- */
-
-/*
- *  Get a free node handle
- */
-rexdd_node_handle   rexdd_new_handle(rexdd_nodeman_p M);
-
-/*
- *  Return a node handle to the free store
- */
-void    rexdd_free_handle(rexdd_nodeman_p M, rexdd_node_handle h);
-
-
-/*
- *  Fill in an unpacked node from a node handle.
- *
- *  return 0 on success...
- */
-int rexdd_unpack_handle(rexdd_unpacked_node_p u,
-        const rexdd_nodeman_p M, rexdd_node_handle h);
-
-
-/*
- *  Fill in a packed node at the specified handle.
- */
-int rexdd_pack_handle(rexdd_nodeman_p M, rexdd_node_handle h,
-        const rexdd_unpacked_node_p u);
 
 
 /*
