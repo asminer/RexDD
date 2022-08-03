724b89649f5e17c5b809e5832639146b4f6b8089
lcdeng <lcdeng@iastate.edu>
2022-08-02 21:34:50 -0500

    Draft of creating RexDD.gv file to view built rexdd

--- tests/check_forest.c
+++ tests/check_forest.c
@@ -36,6 +36,56 @@ void show_unpacked_node(rexdd_forest_t *F, rexdd_unpacked_node_t n)
 
 }
 
+void fprint_rexdd (FILE *f, rexdd_forest_t *F, rexdd_edge_t e)
+{   
+    char label_bufferL[20];
+    char label_bufferH[20];
+
+    rexdd_unpacked_node_t node;
+    rexdd_packed_to_unpacked(rexdd_get_packed_for_handle(F->M,e.target), &node);
+    fprintf(f,"\t%llu [label = \"P_%d\"]\n", e.target, node.level);
+
+    rexdd_snprint_edge(label_bufferL, 20, node.edge[0]);
+    if (rexdd_is_terminal(node.edge[0].target)) {
+        fprintf(f, "\t\"%llu\" -> \"T0\" [style = dashed label = \"%s\"]\n",e.target, label_bufferL);
+    } else {
+        fprintf(f, "\t\"%llu\" -> \"%llu\" [style = dashed label = \"%s\"]\n",
+                    e.target, node.edge[0].target, label_bufferL);
+        fprint_rexdd(f, F, node.edge[0]);
+    }
+
+    rexdd_snprint_edge(label_bufferH, 20, node.edge[1]);
+    if (rexdd_is_terminal(node.edge[1].target)) {
+        fprintf(f, "\t\"%llu\" -> \"T0\" [style = solid label = \"%s\"]\n",e.target, label_bufferH);
+    } else {
+        fprintf(f, "\t\"%llu\" -> \"%llu\" [style = dashed label = \"%s\"]\n",
+                    e.target, node.edge[1].target, label_bufferH);
+        fprint_rexdd(f, F, node.edge[1]);
+    }
+}
+
+void build_gv(rexdd_forest_t *F, rexdd_edge_t e)
+{
+    FILE *f;
+    f = fopen("RexDD.gv", "w+");
+    fprintf(f, "digraph\n{\n");
+
+    char label_buffer[20];
+    rexdd_snprint_edge(label_buffer, 20, e);
+    fprintf(f,"\t0 [shape = point]\n");
+    if (!rexdd_is_terminal(e.target)){
+        fprintf(f, "\t0 -> \"%llu\" [style = solid label = \"%s\"]\n",e.target, label_buffer);
+        fprint_rexdd (f, F, e);
+    } else {
+        fprintf(f, "\t0 -> \"T0\" [style = solid label = \"%s\"]\n", label_buffer);
+    }
+    
+    fprintf(f, "\t\"T0\" [label = \"0\", shape = square]\n");
+
+    fprintf(f,"}");
+    fclose(f);
+}
+
 // void build_nodes(rexdd_forest_t *F, rexdd_unpacked_node_t *root)
 // {
 //     //TBD
@@ -71,11 +121,11 @@ int main()
     uint32_t Levels = 3;
 
     child.level = Levels-1;
-    child.edge[0].target = rexdd_make_terminal(0x06ul);
+    child.edge[0].target = rexdd_make_terminal(0);
     child.edge[0].label.rule = rexdd_rule_EH1;
     child.edge[0].label.complemented = 0;
     child.edge[0].label.swapped = 0;
-    child.edge[1].target = rexdd_make_terminal(0x06ul);
+    child.edge[1].target = rexdd_make_terminal(0);
     child.edge[1].label.rule = rexdd_rule_X;
     child.edge[1].label.complemented = 0;
     child.edge[1].label.swapped = 0;
@@ -83,7 +133,7 @@ int main()
     rexdd_node_handle_t h_child = rexdd_nodeman_get_handle(F.M, &child);
 
     n.level = Levels;
-    n.edge[0].target = rexdd_make_terminal(0x06ul);
+    n.edge[0].target = rexdd_make_terminal(0);
     n.edge[0].label.rule = rexdd_rule_EL0;
     n.edge[0].label.complemented = 1;
     n.edge[0].label.swapped = 0;
@@ -98,20 +148,40 @@ int main()
     show_unpacked_node(&F, n);
     printf("\nnode handle is: %llu\n", h);
     
-    rexdd_reduce_node(&F, &n, &e);
-    printf("\nreduced edge: \n");
-    char buffer[20];
-    rexdd_snprint_edge(buffer, 20, e);
-    printf("\t%s\n", buffer);
-    if (e.target != h) {
-        if (!rexdd_is_terminal(e.target)) {
-            printf("---------Normalized node-------\n");
-            rexdd_unpacked_node_t node;
-            rexdd_packed_to_unpacked(rexdd_get_packed_for_handle(F.M,e.target), &node);
-            show_unpacked_node(&F, node);
-            printf("-------------------------------\n");
-        }
-    }
+    rexdd_edge_label_t l;
+    l.rule = rexdd_rule_X;
+    l.complemented = 0;
+    l.swapped = 0;
+
+    // rexdd_reduce_node(&F, &n, &e);
+    rexdd_reduce_edge(&F, Levels, l, n, &e);
+    
+    /* ==========================================================================
+     *      Old version of printing rexdd
+     * ==========================================================================*/
+    // printf("\nreduced edge: \n");
+    // char buffer[20];
+    // rexdd_snprint_edge(buffer, 20, e);
+    // printf("\t%s\n", buffer);
+    // if (e.target != h) {
+    //     if (!rexdd_is_terminal(e.target)) {
+    //         printf("---------Normalized node-------\n");
+    //         rexdd_unpacked_node_t node;
+    //         rexdd_packed_to_unpacked(rexdd_get_packed_for_handle(F.M,e.target), &node);
+    //         show_unpacked_node(&F, node);
+    //         printf("-------------------------------\n");
+    //     }
+    // }
+
+    /* ==========================================================================
+     *      New version of printing rexdd
+     * ==========================================================================*/
+    printf("\nCreating RexDD.gv file for printing...\n");
+    build_gv(&F, e);
+    printf("Done!\n");
+    printf("Please use command to creat pdf file for viewing:\n");
+    printf("\t\"dot -Tpdf RexDD.gv > RexDD.pdf\"\n");
+
 
     printf("\n=================Checking eval function...===================\n");
 
@@ -122,10 +192,25 @@ int main()
 
     // Build the vars for the specific number of levels
     bool vars[0x01 << num_level][num_level+1];
+    // Build the function results for the specific number of levels
+    bool functions[0x01 << num_level][0x01 << (0x01 << num_level)];
 
     int var = (0x01 << num_level) - 1;
+    int function = (0x01 << (0x01 << num_level)) - 1;
     
     bool out;
+    // for (uint32_t k=0; k<(0x01 << (0x01 << num_level)); k++){
+    //     for (uint32_t i=0; i<(0x01 << num_level); i++) {
+    //         vars[i][0] = 0;
+    //         for (uint32_t j=1; j<(num_level+1); j++) {
+    //             vars[i][j] = var & (0x01<<(j-1));
+    //         }
+    //         var = var-1;
+    //         functions[i][k] = function & (0x01<<(i));
+    //     }
+    //     function = function-1;
+    // }
+
     for (uint32_t i=0; i<(0x01 << num_level); i++) {
         vars[i][0] = 0;
         for (uint32_t j=1; j<(num_level+1); j++) {
