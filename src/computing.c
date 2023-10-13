3dabb3dd77397fe8e32a111e2d499bd002d91c56
lcdeng1 <lichuan.deng97@hotmail.com>
2023-10-13 14:52:34 -0500

    CT code clean

--- src/computing.c
+++ src/computing.c
@@ -184,7 +184,6 @@ void rexdd_sweep_CT(rexdd_comp_table_t *CT, rexdd_nodeman_t *M)
         if (CT->table[i].edgeA == 0) {
             continue;
         } else {
-            if (!rexdd_is_terminal(CT->table[i].edge1->target))
             if ((!rexdd_is_terminal(CT->table[i].edge1->target) && !rexdd_is_packed_marked(rexdd_get_packed_for_handle(M, CT->table[i].edge1->target)))
                 || (!rexdd_is_terminal(CT->table[i].edge2->target) && !rexdd_is_packed_marked(rexdd_get_packed_for_handle(M, CT->table[i].edge2->target)))
                 || (!rexdd_is_terminal(CT->table[i].edgeA->target) && !rexdd_is_packed_marked(rexdd_get_packed_for_handle(M, CT->table[i].edgeA->target))))
